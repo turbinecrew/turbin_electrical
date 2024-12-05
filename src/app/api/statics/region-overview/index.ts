06684dccfccf98e5c8f26c5d38228370acc90867
SeongYun_Cheon <1112csy@naver.com>
2024-12-05 16:51:04 +0900

    🔧 지역별 발전량 api 연결 코드 보완

--- src/app/api/statics/region-overview/index.ts
+++ src/app/api/statics/region-overview/index.ts
@@ -1,23 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
-import axios from "axios";
+import axios from "axios"
 
-export async function GET(req: NextRequest) {
-  try {
-    const API_URL = "https://apis.data.go.kr/B552115/PvAmountByLocHr/getPvAmountByLocHr";
-    const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+export async function GetRegionOverviewStatics() {
+    const API_URL =
+        "https://apis.data.go.kr/B552115/PvAmountByLocHr/getPvAmountByLocHr"
+    const API_KEY = process.env.NEXT_PUBLIC_API_KEY
+    try {
+        const response = await axios.get(API_URL, {
+            params: {
+                serviceKey: decodeURIComponent(API_KEY!),
+                pageNo: 1,
+                numOfRows: 384,
+                dataType: "json",
+            },
+        })
 
-    const response = await axios.get(API_URL, {
-      params: {
-        serviceKey: decodeURIComponent(API_KEY!),
-        pageNo: 1,
-        numOfRows: 384,
-        dataType: "json",
-      },
-    });
-
-    return NextResponse.json(response.data);
-  } catch (error) {
-    console.error("API Route Error:", error);
-    return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
-  }
+        return response.data
+    } catch (error) {
+        return console.error("API Route Error:", error)
+    }
 }
