516fc2c904572a1164fc0453c877ba8e4e8b8e4b
Giradon <sch6523warz@outlook.kr>
2024-12-06 13:32:13 +0900

    🔧: tailwind config 파일 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ export default {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/common/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
