271f46f925849fe588adec593c170e38c4c8f450
narim <0018823nmn@naver.com>
2024-12-08 05:30:37 +0900

    ⚡️ : getData() 분리

---
+++ src/app/(main)/trading/trading-table/data.tsx
@@ -0,0 +1,128 @@
+import { Payment } from "./columns"
+async function getData(): Promise<Payment[]> {
+	// Fetch data from your API here.
+	return [
+		{
+			id: "yx4bk68g",
+			amount: 446,
+			status: "processing",
+			email: "konwn@demo.com",
+		},
+		{
+			id: "wkv1h06c",
+			amount: 302,
+			status: "success",
+			email: "vqxfl@example.com",
+		},
+		{
+			id: "ro2fp0bt",
+			amount: 447,
+			status: "failed",
+			email: "yqnla@test.com",
+		},
+		{
+			id: "kenhcijs",
+			amount: 460,
+			status: "failed",
+			email: "lijks@example.com",
+		},
+		{
+			id: "ssv4lwpn",
+			amount: 66,
+			status: "pending",
+			email: "daaef@mail.com",
+		},
+		{
+			id: "h1cwqg2p",
+			amount: 430,
+			status: "processing",
+			email: "gmikt@mail.com",
+		},
+		{
+			id: "jripkyc3",
+			amount: 486,
+			status: "processing",
+			email: "fjete@demo.com",
+		},
+		{
+			id: "iq3yrdto",
+			amount: 451,
+			status: "failed",
+			email: "vnadu@example.com",
+		},
+		{
+			id: "55ybwd4n",
+			amount: 467,
+			status: "pending",
+			email: "jtdja@demo.com",
+		},
+		{
+			id: "0egd0394",
+			amount: 314,
+			status: "success",
+			email: "wakyk@mail.com",
+		},
+		{
+			id: "tnir6xju",
+			amount: 380,
+			status: "success",
+			email: "ftkwp@demo.com",
+		},
+		{
+			id: "ayyc0bsv",
+			amount: 205,
+			status: "pending",
+			email: "mgckn@example.com",
+		},
+		{
+			id: "jyse4ih8",
+			amount: 239,
+			status: "success",
+			email: "eutqg@example.com",
+		},
+		{
+			id: "y72z4j1p",
+			amount: 348,
+			status: "processing",
+			email: "fqsne@demo.com",
+		},
+		{
+			id: "sp14i2aq",
+			amount: 354,
+			status: "pending",
+			email: "pbljz@test.com",
+		},
+		{
+			id: "v9lrdrt8",
+			amount: 127,
+			status: "processing",
+			email: "hfyyv@demo.com",
+		},
+		{
+			id: "6jyid7xn",
+			amount: 205,
+			status: "processing",
+			email: "uwdop@mail.com",
+		},
+		{
+			id: "jc1u6y9e",
+			amount: 205,
+			status: "success",
+			email: "hodpa@test.com",
+		},
+		{
+			id: "rqrvf4a0",
+			amount: 462,
+			status: "processing",
+			email: "emnvu@test.com",
+		},
+		{
+			id: "09w1ic8o",
+			amount: 207,
+			status: "success",
+			email: "vjedj@mail.com",
+		},
+	]
+}
+
+export default getData
--- src/app/(main)/trading/trading-table/page.tsx
+++ src/app/(main)/trading/trading-table/page.tsx
@@ -1,17 +1,6 @@
-import { Payment, columns } from "./columns"
+import { columns } from "./columns"
 import { DataTable } from "./data-table"
-
-async function getData(): Promise<Payment[]> {
-	// Fetch data from your API here.
-	return [
-		{
-			id: "728ed52f",
-			amount: 100,
-			status: "pending",
-			email: "m@example.com",
-		},
-	]
-}
+import getData from "./data"
 
 export default async function DemoPage() {
 	const data = await getData()
