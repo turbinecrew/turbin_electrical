ceeb49521207634e12acbb57eded06daaf2e88cf
narim <0018823nmn@naver.com>
2024-12-08 16:00:24 +0900

    ✨ : trading 중앙 table (거래 상세 정보)에 맞게 데이터 추가하고 컬럼 수정

--- src/app/(main)/trading/trading-table/columns.tsx
+++ src/app/(main)/trading/trading-table/columns.tsx
@@ -6,43 +6,92 @@ import { ArrowUpDown } from "lucide-react"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
+export type EnergyTrade = {
 	id: string
-	amount: number
-	status: "pending" | "processing" | "success" | "failed"
-	email: string
+	tradeTime: string // 거래 시간
+	plantName: string // 발전소 이름
+	volume: string // 거래량
+	bidPrice: string // 입찰 가격
+	matchingStatus: "matched" | "unmatched" // 매칭 여부
 }
 
-export const columns: ColumnDef<Payment>[] = [
+export const columns: ColumnDef<EnergyTrade>[] = [
 	{
-		accessorKey: "status",
-		header: "Status",
+		accessorKey: "tradeTime",
+		header: ({ column }) => {
+			return (
+				<div className=" ">
+					<Button
+						className="border-none hover:bg-gray-200"
+						onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+					>
+						tradeTime
+						<ArrowUpDown className="ml-2 h-4 w-4" />
+					</Button>
+				</div>
+			)
+		},
+		cell: ({ row }) => {
+			const tradeTime = new Date(row.getValue("tradeTime"))
+			const formatted = new Intl.DateTimeFormat("ko-KR", {
+				dateStyle: "medium",
+				timeStyle: "short",
+				hour12: false,
+			}).format(tradeTime)
+
+			return <div className="text-start font-medium">{formatted}</div>
+		},
 	},
 	{
-		accessorKey: "email",
+		accessorKey: "plantName",
+		header: "plantName",
+	},
+	{
+		accessorKey: "volume",
 		header: ({ column }) => {
 			return (
 				<Button
 					className="border-none hover:bg-gray-200"
 					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
 				>
-					Email
+					volume
 					<ArrowUpDown className="ml-2 h-4 w-4" />
 				</Button>
 			)
 		},
+		cell: ({ row }) => {
+			const volume = parseFloat(row.getValue("volume"))
+			const formatted = new Intl.NumberFormat("ko-KR", {}).format(volume)
+
+			return <div className="text-start font-medium">{formatted}MWh</div>
+		},
 	},
 	{
-		accessorKey: "amount",
-		header: () => <div className="text-right">Amount</div>,
-		cell: ({ row }) => {
-			const amount = parseFloat(row.getValue("amount"))
-			const formatted = new Intl.NumberFormat("en-US", {
-				style: "currency",
-				currency: "USD",
-			}).format(amount)
-
-			return <div className="text-right font-medium">{formatted}</div>
+		accessorKey: "bidPrice",
+		header: ({ column }) => {
+			return (
+				<Button
+					className="border-none hover:bg-gray-200"
+					onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+				>
+					bidPrice
+					<ArrowUpDown className="ml-2 h-4 w-4" />
+				</Button>
+			)
 		},
+		cell: ({ row }) => {
+			const bidPrice = parseFloat(row.getValue("bidPrice"))
+			const formatted = new Intl.NumberFormat("ko-KR", {
+				style: "currency",
+				currency: "krw",
+				currencyDisplay: "narrowSymbol",
+			}).format(bidPrice)
+
+			return <div className="text-start font-medium">{formatted}/kWh</div>
+		},
+	},
+	{
+		accessorKey: "matchingStatus",
+		header: "matchingStatus",
 	},
 ]
