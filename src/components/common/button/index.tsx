605566b06867e17e2566c938176e78c71a1f6056
narim <0018823nmn@naver.com>
2024-12-09 16:20:56 +0900

    🎨 : 버튼 컴포넌트 추가

---
+++ src/components/common/button/index.tsx
@@ -0,0 +1,20 @@
+import cn from "clsx"
+import { ButtonHTMLAttributes } from "react"
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	className?: string
+}
+const Button = (props: ButtonProps) => {
+	const { className, ...rest } = props
+	return (
+		<button
+			{...rest}
+			className={cn(
+				className,
+				"inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md border border-input bg-background text-sm font-medium ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+			)}
+		>
+			{props.children}
+		</button>
+	)
+}
+export default Button
