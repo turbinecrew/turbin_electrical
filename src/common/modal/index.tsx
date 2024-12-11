5cbc0c1c83d15bcbc942203e3a3ec1f9e1d957cb
SeongYun_Cheon <1112csy@naver.com>
2024-12-11 15:20:00 +0900

    ⚡️: 모달 불필요 코드 삭제 (내부 클릭 방지 코드)

--- src/common/modal/index.tsx
+++ src/common/modal/index.tsx
@@ -18,10 +18,7 @@ export const Modal = ({ isOpen, setIsOpen, children }: ModalPT) => {
 				if (e.target === e.currentTarget) setIsOpen(false)
 			}}
 		>
-			<div
-				className="relative w-full max-w-md rounded-xl bg-white shadow-lg"
-				onClick={(e) => e.stopPropagation()}
-			>
+			<div className="relative w-full max-w-md rounded-xl bg-white shadow-lg">
 				<button
 					className="absolute right-4 top-4 text-gray-500 hover:text-gray-800"
 					onClick={() => setIsOpen(false)}
