ae053143a31fc06060c8b777f843091e39c1bb0c
SeongYun_Cheon <1112csy@naver.com>
2024-12-17 12:36:40 +0900

    🔥: 관리자 페이지  불필요 코드 삭제 (미사용import 삭제)

--- src/app/(main)/manage/page.tsx
+++ src/app/(main)/manage/page.tsx
@@ -1,13 +1,7 @@
 "use client"
 
 import React, { useState, useEffect } from "react"
-import {
-	Card,
-	CardHeader,
-	CardTitle,
-	CardDescription,
-	CardContent,
-} from "@/components/ui/card"
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card"
 
 type Bid = {
 	id: number
