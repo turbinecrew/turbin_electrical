c9064fb1cabd2bad815125e298a1085c9b0b121b
SeongYun_Cheon <1112csy@naver.com>
2024-12-18 16:41:32 +0900

    ✨: 각 지역의 시간별 그래프 모달 띄우기기

---
+++ src/features/auth/components/regional-modal/index.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import {
+	Line,
+	LineChart,
+	XAxis,
+	YAxis,
+	CartesianGrid,
+	Tooltip,
+	ResponsiveContainer,
+} from "recharts"
+
+import {
+	Modal,
+	ModalHeader,
+	ModalContext,
+	ModalFooter,
+} from "@/common/components/modal"
+
+type RegionalModalProps = {
+	isOpen: boolean
+	setIsOpen: (isOpen: boolean) => void
+	region: string | null
+	timeSeriesData: any[]
+}
+
+export function RegionalModal({
+	isOpen,
+	setIsOpen,
+	region,
+	timeSeriesData,
+}: RegionalModalProps) {
+	return (
+		<Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+			<ModalHeader>{region} 시간별 발전량</ModalHeader>
+			<ModalContext>
+				<div className="h-[250px] w-full">
+					<ResponsiveContainer width="100%" height="100%">
+						<LineChart
+							data={timeSeriesData}
+							margin={{ top: 20, right: 30, left: 20, bottom: 20 }}
+						>
+							<CartesianGrid strokeDasharray="3 3" />
+							<XAxis
+								dataKey="시간"
+								tick={{ fontSize: 12 }}
+								label={{ value: "시간", position: "bottom" }}
+							/>
+							<YAxis
+								tick={{ fontSize: 12 }}
+								label={{
+									value: "발전량(kW)",
+									angle: -90,
+									position: "insideLeft",
+								}}
+							/>
+							<Tooltip />
+							<Line type="monotone" dataKey="발전량(kW)" stroke="#07A525" />
+						</LineChart>
+					</ResponsiveContainer>
+				</div>
+			</ModalContext>
+			<ModalFooter>
+				<button className="rounded-lg bg-green-500 px-4 py-2 text-white">
+					거래하기
+				</button>
+			</ModalFooter>
+		</Modal>
+	)
+}
