abaf290a06e1f3b758c79747c9ae4287b412cd65
Giradon <sch6523warz@outlook.kr>
2024-12-31 23:20:58 +0900

    🎨 : 주간 예측 차트 디자인 변경

---
+++ src/features/prediction/components/chart/daily/mocks.ts
@@ -0,0 +1,9 @@
+import type { DotsLineChartData } from "@/common/components/chart/line-chart/Dots/DotsLineChart"
+
+export const WeeklyWeatherData: DotsLineChartData[] = [
+	{ 날짜: "17일", 발전량: 500 },
+	{ 날짜: "18일", 발전량: 1200 },
+	{ 날짜: "19일", 발전량: 900 },
+	{ 날짜: "20일", 발전량: 1000 },
+	{ 날짜: "21일", 발전량: 800 },
+]
