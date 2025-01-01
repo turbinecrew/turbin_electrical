a9e1f15b88d74cdb6f79597e9210e2844ed97428
seongyun4359 <1112csy@naver.com>
2025-01-01 12:34:14 +0900

    🎨: 사이드바 토글 off 다자인 및 active 상태 디자인

--- src/common/components/sidebar/index.tsx
+++ src/common/components/sidebar/index.tsx
@@ -25,6 +25,8 @@ const accountNavItems = [
 
 export default function Sidebar() {
 	const [isSidebarVisible, setIsSidebarVisible] = useState(true)
+	const [activeItem, setActiveItem] = useState<string>("Dashboard")
+
 	const toggleSidebar = () => setIsSidebarVisible((prev) => !prev)
 
 	return (
@@ -51,23 +53,31 @@ export default function Sidebar() {
 								width={isSidebarVisible ? 200 : 40}
 								height={isSidebarVisible ? 70 : 40}
 								priority
-								className="cursor-pointer rounded-lg"
+								className={`cursor-pointer rounded-lg ${
+									isSidebarVisible ? "my-0" : "my-[8px]"
+								}`}
 							/>
 						</Link>
 					</div>
+
 					<hr className="mb-6 w-full border-gray-300" />
-					<nav className="flex-1">
+
+					<nav className="w-full flex-1 px-1">
 						<ul className="space-y-4">
 							{mainNavItems.map((item) => (
 								<NavItem
 									key={item.title}
 									item={item}
 									isSidebarVisible={isSidebarVisible}
+									isActive={activeItem === item.title}
+									onClick={() => setActiveItem(item.title)}
 								/>
 							))}
 						</ul>
-						<hr className="my-6 self-stretch border-gray-300" />
-						<div>
+
+						<hr className="mx-4 my-6 border-gray-300" />
+
+						<div className="ml-2">
 							<h3
 								className={`mb-4 text-sm font-semibold text-gray-600 ${
 									isSidebarVisible ? "block" : "hidden"
@@ -81,13 +91,16 @@ export default function Sidebar() {
 										key={item.title}
 										item={item}
 										isSidebarVisible={isSidebarVisible}
+										isActive={activeItem === item.title}
+										onClick={() => setActiveItem(item.title)}
 									/>
 								))}
 							</ul>
 						</div>
 					</nav>
-					<div className="mt-auto w-full">
-						<div className="mb-4 flex justify-start px-4">
+
+					<div className="mt-auto w-full px-1">
+						<div className="mb-4 flex justify-start">
 							<button
 								onClick={toggleSidebar}
 								className="flex h-10 w-10 items-center justify-center text-gray-700 shadow-sm hover:bg-gray-100"
@@ -105,6 +118,8 @@ export default function Sidebar() {
 function NavItem({
 	item,
 	isSidebarVisible,
+	isActive,
+	onClick,
 }: {
 	item: {
 		title: string
@@ -112,15 +127,32 @@ function NavItem({
 		icon: React.ComponentType<{ className: string }>
 	}
 	isSidebarVisible: boolean
+	isActive: boolean
+	onClick: () => void
 }) {
 	const Icon = item.icon
 	return (
 		<li>
 			<Link
 				href={item.url}
-				className="group flex items-center rounded-md px-6 py-2 text-gray-700 transition-colors duration-300 hover:bg-tbPastelGreen hover:text-white"
+				onClick={onClick}
+				className={`group flex items-center rounded-md px-4 py-2 text-gray-700 transition-colors duration-300 ${
+					isActive
+						? "bg-tbPastelGreen text-white"
+						: isSidebarVisible
+							? "hover:bg-tbPastelGreen hover:text-white"
+							: "justify-center hover:bg-tbGreen hover:text-white"
+				}`}
 			>
-				<div className="flex h-10 w-10 items-center justify-center rounded-full bg-white text-tbGreen group-hover:bg-white group-hover:text-tbGreen">
+				<div
+					className={`flex h-10 w-10 items-center justify-center rounded-full duration-300 ${
+						isActive
+							? "bg-tbPastelGreen text-tbGreen"
+							: isSidebarVisible
+								? "bg-white text-tbGreen group-hover:bg-white group-hover:text-tbGreen"
+								: "bg-white text-tbGreen group-hover:bg-tbGreen group-hover:text-white"
+					}`}
+				>
 					<Icon className="h-5 w-5" />
 				</div>
 				{isSidebarVisible && (
