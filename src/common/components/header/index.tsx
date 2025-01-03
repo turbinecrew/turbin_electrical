89dd5bd80864ff02d5689a0dd83eac41697e19ec
narim <0018823nmn@naver.com>
2025-01-03 23:52:00 +0900

    🎨 : 반응형 디자인 추가 작업

--- src/common/components/header/index.tsx
+++ src/common/components/header/index.tsx
@@ -30,58 +30,62 @@ export default function Header() {
 	}
 
 	return (
-		<div className="flex h-20 w-full items-center justify-between border-b border-gray-200 bg-white p-4 shadow-sm">
+		<div className="flex h-16 w-full items-center justify-between border-b border-gray-200 bg-white p-4 shadow-sm md:h-20">
 			<div className="flex items-center gap-2">
-				<nav className="flex items-center gap-2 opacity-40">
-					<Link href="/" className="flex items-center hover:text-tbGreen">
-						<House size={16} />
-					</Link>
-					{prevPage && prevPage != "" && (
-						<div className="flex items-center gap-2 capitalize">
-							<Slash size={16} />{" "}
-							<a
-								href={`/${prevPage}`}
-								className="flex items-center hover:text-tbGreen"
-							>
-								{prevPage}
-							</a>
-						</div>
-					)}
-				</nav>
-				<div className="flex items-center gap-2 text-lg font-semibold text-gray-800">
-					<Slash size={16} />
-					<span className="capitalize">
+				<nav className="flex items-center gap-2">
+					<div className="flex items-center gap-2 opacity-40">
+						<Link href="/" className="flex items-center hover:text-tbGreen">
+							<House size={16} />
+						</Link>
+						{prevPage && prevPage != "" && (
+							<div className="flex items-center gap-2">
+								<Slash size={14} className="mt-[1px] rotate-[-15deg]" />
+								<a
+									href={`/${prevPage}`}
+									className="text-sm capitalize hover:text-tbGreen md:text-lg"
+								>
+									{prevPage}
+								</a>
+							</div>
+						)}
+					</div>
+					<div className="flex items-center gap-2 text-sm font-semibold capitalize md:text-lg">
+						<Slash size={14} className="mt-[1px] rotate-[-15deg]" />
 						{thisPage === "main" || thisPage === "" ? "Dashboard" : thisPage}
-					</span>
-				</div>
+					</div>
+				</nav>
 			</div>
-			<div className="flex items-center gap-2">
-				<div className="flex h-10 items-center rounded-2xl border border-gray-300 bg-gray-50 px-4 focus-within:border-tbGreen">
-					<Search className="text-gray-400" size={16} />
+			<div className="flex flex-row items-center gap-2 pl-2 md:gap-4">
+				<div className="flex h-6 items-center rounded-2xl border border-gray-300 bg-gray-50 px-4 focus-within:border-tbGreen md:h-8">
+					<Search size={12} className="text-gray-400 md:h-4 md:w-4" />
 					<input
 						type="text"
-						className="ml-2 h-full w-48 border-none bg-transparent text-gray-600 placeholder-gray-400 outline-none"
+						className="ml-2 h-full w-10 border-none bg-transparent text-xs text-gray-600 placeholder-gray-400 outline-none md:w-36 md:text-sm"
 						placeholder="Search..."
 						value={searchText}
 						onChange={handleInputChange}
 						onKeyDown={handleKeyDown}
 					/>
 				</div>
-				<div
-					className="flex cursor-pointer items-center gap-2 text-gray-700 hover:text-tbGreen"
-					onClick={toggleSignInModal}
-				>
-					<CircleUser size={20} />
-					<span className="text-sm font-medium">Sign In</span>
+				<div className="flex gap-2">
+					<div
+						className="flex cursor-pointer items-center gap-2 text-gray-700 hover:text-tbGreen"
+						onClick={toggleSignInModal}
+					>
+						<CircleUser size={12} className="md:h-4 md:w-4" />
+						<span className="text-nowrap text-center text-xs font-medium md:text-sm">
+							Sign In
+						</span>
+					</div>
+					<Settings
+						size={16}
+						className="cursor-pointer text-gray-700 hover:text-tbGreen md:h-5 md:w-5"
+					/>
+					<Bell
+						size={16}
+						className="cursor-pointer text-gray-700 hover:text-tbGreen md:h-5 md:w-5"
+					/>
 				</div>
-				<Settings
-					size={20}
-					className="cursor-pointer text-gray-700 hover:text-tbGreen"
-				/>
-				<Bell
-					size={20}
-					className="cursor-pointer text-gray-700 hover:text-tbGreen"
-				/>
 			</div>
 			<SignInModal
 				isOpen={isSignInModalOpen}
