d25a50343f934bf45b42a106aa40f8c84dea032d
Giradon <sch6523warz@outlook.kr>
2025-01-04 17:10:38 +0900

    🏷️: bar chart type 수정

--- src/features/region/components/bar-chart/index.tsx
+++ src/features/region/components/bar-chart/index.tsx
@@ -10,31 +10,10 @@ import {
 	CardTitle,
 } from "@/shadcn/components/card"
 
-const chartData = [
-	{ month: "January", desktop: 186, mobile: 80 },
-	{ month: "February", desktop: 305, mobile: 200 },
-	{ month: "March", desktop: 237, mobile: 120 },
-	{ month: "April", desktop: 73, mobile: 190 },
-	{ month: "May", desktop: 209, mobile: 130 },
-	{ month: "June", desktop: 214, mobile: 140 },
-]
+import type { MockChartPT } from "./type"
+import { chartConfig, chartData } from "./type"
 
-const chartConfig = {
-	desktop: {
-		label: "Desktop",
-		color: "hsl(var(--chart-1))",
-	},
-	mobile: {
-		label: "Mobile",
-		color: "hsl(var(--chart-2))",
-	},
-}
-
-type MockChartProps = {
-	cardTitle: string
-}
-
-export function MockChart({ cardTitle }: MockChartProps) {
+export function MockChart({ cardTitle }: MockChartPT) {
 	return (
 		<Card>
 			<CardHeader>
