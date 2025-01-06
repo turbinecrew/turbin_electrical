8b164f2417e646bb24e760694036d70a1a2b03e2
narim <0018823nmn@naver.com>
2025-01-07 04:15:22 +0900

    🚚 : data 분리

--- src/common/components/notification-center/index.tsx
+++ src/common/components/notification-center/index.tsx
@@ -3,102 +3,10 @@
 import { Bell, Trash2, SquareCheckBig, Square } from "lucide-react"
 import { useState, useEffect, useRef } from "react"
 
+import { notificationData } from "@/common/components/notification-center/data"
 import { IconFormatter } from "@/common/components/notification-center/icon"
 
 import styles from "./style/notification.module.css"
-
-const notificationData = [
-	{
-		id: "12314",
-		type: "notice",
-		title: "알림입니다",
-		message: "알림상세메시지",
-		link: "/trading",
-		read: true,
-		date: "2025-01-05 12:00",
-		deleted_at: "",
-	},
-	{
-		id: "23425",
-		type: "warnig",
-		title: "중요 공지사항",
-		message: "시스템 점검 예정입니다",
-		link: "",
-		read: false,
-		date: "2025-01-06 09:30",
-		deleted_at: "",
-	},
-	{
-		id: "34536",
-		type: "alram",
-		title: "업데이트 안내",
-		message: "새로운 기능이 추가되었습니다",
-		link: "",
-		read: true,
-		date: "2025-01-06 15:45",
-		deleted_at: "",
-	},
-	{
-		id: "45647",
-		type: "region page",
-		title: "긴급 알림",
-		message: "서버 오류가 발생했습니다",
-		link: "",
-		read: false,
-		date: "2025-01-07 03:20",
-		deleted_at: "",
-	},
-	{
-		id: "56758",
-		type: "notice",
-		title: "이벤트 안내",
-		message: "신년 맞이 특별 이벤트 시작",
-		link: "",
-		read: false,
-		date: "2025-01-07 10:00",
-		deleted_at: "",
-	},
-	{
-		id: "34526",
-		type: "alram",
-		title: "업데이트 안내",
-		message: "새로운 기능이 추가되었습니다",
-		link: "",
-		read: false,
-		date: "2025-01-07 15:45",
-		deleted_at: "",
-	},
-	{
-		id: "33526",
-		type: "alram",
-		title: "업데이트 안내",
-		message: "새로운 기능이 추가되었습니다",
-		link: "",
-		read: false,
-		date: "2025-01-07 15:45",
-		deleted_at: "",
-	},
-	{
-		id: "163414",
-		type: "notice",
-		title: "알림입니다",
-		message: "알림상세메시지",
-		link: "/trading",
-		read: false,
-		date: "2025-01-05 12:00",
-		deleted_at: "",
-	},
-	{
-		id: "12886666",
-		type: "notice",
-		title: "알림입니다",
-		message: "알림상세메시지",
-		link: "/trading",
-		read: false,
-		date: "2025-01-05 12:00",
-		deleted_at: "",
-	},
-]
 
 export function NotificationPopup() {
 	const [notifications, setNotifications] = useState(notificationData)
