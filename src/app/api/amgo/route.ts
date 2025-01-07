ffea4ab928106eb67e9ff93340d949b292bdce57
keeprok <140785214+keeprok@users.noreply.github.com>
2025-01-07 23:26:46 +0900

    ✨ : mongoDb db 각데이터 별 연결

---
+++ src/app/api/amgo/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server"
+
+import { connectDB } from "@/app/api/mongodb"
+
+export async function GET() {
+	try {
+		const db = await connectDB()
+		if (!db) {
+			return NextResponse.json(
+				{ message: "AMGO_Database connection failed" },
+				{ status: 500 },
+			)
+		}
+		const collection = db.collection("amgo")
+		const data = await collection.find({}).toArray()
+
+		return NextResponse.json(data, { status: 200 })
+	} catch (err) {
+		// 에러 처리
+		console.error("발전량 데이터 조회 실패", err)
+		return NextResponse.json(
+			{
+				message: "서버 오류 발생",
+				error: err instanceof Error ? err.message : "Unknown error",
+			},
+			{ status: 500 },
+		)
+	}
+}
