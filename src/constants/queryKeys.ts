0bbe613eb56db4c6f0f040956be79c55b89575ff
seongyun4359 <1112csy@naver.com>
2025-01-08 21:59:07 +0900

    🐛: 쿼리키 수신으로 변경

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -1,7 +1,4 @@
 export enum QUERY_KEYS {
-	REC_DATA = "recData",
-	TodaySMPData = "smpData",
-	AMGO_DATA = "amgoData",
 	PAST_REC = "get-past-REC",
 	PAST_SMP = "get-past-SMP",
 	TODAY_SMP = "get-today-SMP",
