2c5de2a63c24ad7f52b69f10db6c8d3afeb92886
Giradon <100326052+Giradon@users.noreply.github.com>
2025-01-09 13:44:59 +0900

    ⚡️: common/bar-chart 수정

--- src/common/components/chart/bar-chart/index.tsx
+++ src/common/components/chart/bar-chart/index.tsx
@@ -1,87 +1,50 @@
 "use client"
 
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts"
+
 import {
-	Bar,
-	BarChart,
-	CartesianGrid,
-	XAxis,
-	YAxis,
-	ResponsiveContainer,
-	Tooltip,
-} from "recharts"
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+} from "@/shadcn/components/chart"
 
-// import { Skeleton } from "@/common/components/skeleton"
-import { useMockData } from "@/features/region/hooks/data/useMockData"
-
-import type { BarChartComponentPT } from "./BarChart"
+import type { BarChartComponentPT } from "./type"
 
 export function BarChartComponent({
-	data,
-	xAxisKey,
-	dataKeys,
-	config,
+	chartConfig,
+	chartData,
+	BarDataKey,
+	XAxisDataKey,
+	Ymin,
+	Ymax,
 }: BarChartComponentPT) {
-	const { data: chartData } = useMockData(data)
-
-	// if (isLoading) {
-	// 	return (
-	// 		<div className="h-96 w-full p-4">
-	// 			<Skeleton className="h-[300px] w-full rounded-lg" />
-	// 		</div>
-	// 	)
-	// }
-
 	return (
-		<div className="h-96 w-full p-4">
-			<ResponsiveContainer width="100%" height={300}>
-				<BarChart
-					accessibilityLayer
-					data={chartData}
-					margin={{ top: 5, right: 20, left: 20, bottom: 5 }}
-				>
-					<CartesianGrid
-						vertical={false}
-						strokeDasharray="3 3"
-						stroke="#f0f0f0"
-						strokeWidth={1}
-					/>
-					<XAxis
-						dataKey={xAxisKey}
-						tickLine={false}
-						tickMargin={10}
-						axisLine={false}
-					/>
-					<YAxis
-						tickCount={10}
-						axisLine={false}
-						tickLine={false}
-						tickMargin={10}
-						domain={[0, "auto"]}
-					/>
-					<Tooltip
-						cursor={false}
-						contentStyle={{
-							backgroundColor: "#fff",
-							border: "1px solid #ccc",
-							borderRadius: "4px",
-						}}
-					/>
-					<Bar
-						dataKey={dataKeys.primary}
-						fill={config.primary.color}
-						radius={4}
-						barSize={20}
-					/>
-					{dataKeys.secondary && config.secondary && (
-						<Bar
-							dataKey={dataKeys.secondary}
-							fill={config.secondary.color}
-							radius={4}
-							barSize={20}
-						/>
-					)}
-				</BarChart>
-			</ResponsiveContainer>
-		</div>
+		<ChartContainer config={chartConfig}>
+			<BarChart data={chartData}>
+				<CartesianGrid vertical={false} />
+				<XAxis
+					dataKey={XAxisDataKey}
+					tickLine={false}
+					tickMargin={10}
+					axisLine={false}
+					tickFormatter={(value) => value.slice(0, 3)}
+				/>
+				<YAxis
+					domain={[Ymin, Ymax]}
+					axisLine={false}
+					tickLine={false}
+					tickMargin={10}
+				/>
+				<ChartTooltip
+					cursor={false}
+					content={<ChartTooltipContent indicator="dashed" />}
+				/>
+				<Bar
+					dataKey={BarDataKey}
+					fill={`var(--color-${BarDataKey})`}
+					radius={4}
+				/>
+			</BarChart>
+		</ChartContainer>
 	)
 }
