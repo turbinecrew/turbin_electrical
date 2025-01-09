8d3c0d1566f0a853cd71ad4229feb58a92f03ce2
seongyun4359 <1112csy@naver.com>
2025-01-09 13:21:32 +0900

    🐛: 쿼리키 수정

--- src/features/summary-data/hook/useTodaySMPData.ts
+++ src/features/summary-data/hook/useTodaySMPData.ts
@@ -7,7 +7,7 @@ import { fetchTodaySMPData } from "../api/fetchTodaySMPData"
 
 export const useTodaySMPData = () => {
 	return useQuery({
-		queryKey: [QUERY_KEYS.TodaySMPData], // 고유한 키
+		queryKey: [QUERY_KEYS.TODAY_SMP], // 고유한 키
 		queryFn: fetchTodaySMPData, // 데이터를 가져오는 함수
 		staleTime: 1000 * 60 * 5, // 5분 동안 캐시 유지
 		retry: 3, // 실패 시 재시도
