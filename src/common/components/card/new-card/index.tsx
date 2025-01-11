97bf551b33f85c41a5291101215c6364ef7d793a
narim <0018823nmn@naver.com>
2025-01-11 18:31:21 +0900

    ⚡️ : 공용 카드컴포넌트 리팩토링

--- src/common/components/card/new-card/index.tsx
+++ src/common/components/card/new-card/index.tsx
@@ -3,122 +3,117 @@ import { cn } from "@/util/utils"
 
 type CardPT = {
 	className?: string
+	children?: ReactNode
 	isColored?: boolean
-	ref?: LegacyRef<HTMLDivElement>
 }
-const tbCard = ({ ref, className, isColored = false }: CardPT) => {
+export function TbCard({ className, children, isColored = false }: CardPT) {
 	return (
 		<div
-			ref={ref}
 			className={cn("rounded-2xl shadow-md", className, {
 				"bg-tbPastelGreen": isColored,
 				"bg-[#FAFAFA]": !isColored,
 			})}
-		/>
-	)
-}
-
-const tbCardTitle = ({ className, ref }: CardPT) => {
-	return (
-		<div
-			ref={ref}
-			className={cn(className, "text-start text-lg font-bold text-black")}
-		/>
-	)
-}
-
-const tbCardSubTitle = ({ className, ref }: CardPT) => {
-	return (
-		<div
-			ref={ref}
-			className={cn(
-				className,
-				"mb-1 text-start text-sm font-normal text-black/75",
-			)}
-		/>
-	)
-}
-
-const tbCardHeader = ({ className, ref }: CardPT) => {
-	return (
-		<div
-			ref={ref}
-			className={cn(
-				className,
-				"flex flex-col gap-3 space-y-1.5 p-3 md:gap-5 md:p-5",
-			)}
-		/>
-	)
-}
-
-const tbCardContent = ({ className, ref }: CardPT) => {
-	return <div ref={ref} className={cn(className, "p-3 pt-0 md:p-5")} />
-}
-
-const DividedArea = ({ className }: CardPT) => {
-	return <div className={cn(className, "flex h-fit justify-between")} />
-}
-
-export type MiniCardPT2 = {
-	title: string
-	value: string | number
-	unit?: string
-	isColored?: boolean
-	isIncreased?: boolean
-	amount?: number
-	className?: string
-	color?: string
-}
-
-const MiniCard2 = ({
-	title,
-	value,
-	unit,
-	amount,
-	isIncreased,
-	className,
-	color,
-}: MiniCardPT) => {
-	return (
-		<div
-			className={cn(
-				className,
-				"w-full flex-col gap-2 rounded-xl p-6 shadow-md",
-			)}
 		>
-			<div className="text-xs font-semibold">{title}</div>
-
-			<div className="flex items-center justify-between">
-				<div className="flex items-baseline gap-1">
-					<span className="scroll-m-20 text-2xl font-semibold tracking-tight">
-						{value}
-					</span>
-					<span className="scroll-m-20 text-lg font-semibold tracking-tight">
-						{unit}
-					</span>
-				</div>
-				{(isIncreased != null || "") &&
-					(isIncreased ? (
-						<div className="text-xs font-bold text-red-600">+{amount}</div>
-					) : (
-						<div className="text-xs font-bold text-blue-600">-{amount}</div>
-					))}
-			</div>
+			{children}
 		</div>
 	)
 }
 
-tbCardTitle.displayNme = "tbCardTitle"
-tbCardSubTitle.displayNme = "tbCardSubTitle"
-tbCardHeader.displayNme = "tbCardHeader"
-tbCardContent.displayNme = "tbCardContent"
-DividedArea.displayNme = "DividedArea"
+type TitlePT = {
+	className?: string
+	children?: React.ReactNode
+	title?: string
+	rightArea?: React.ReactNode
+	lowerTitle?: string
+}
 
-export {
-	tbCard,
-	tbCardTitle,
-	tbCardSubTitle,
-	tbCardHeader,
-	tbCardContent,
-	MiniCard2,
+export function TbCardHeader({
+	className,
+	children,
+	title,
+	rightArea,
+	lowerTitle,
+}: TitlePT) {
+	return (
+		<div
+			className={cn(
+				className,
+				"flex flex-col gap-3 space-y-1.5 p-3 md:gap-5 md:p-5",
+			)}
+		>
+			<div className="flex flex-col">
+				{title && (
+					<div className="flex h-fit justify-between">
+						<span className="text-start text-lg font-bold text-black">
+							{title}
+						</span>
+						{rightArea}
+					</div>
+				)}
+				{lowerTitle && (
+					<span className="mb-1 text-start text-sm font-normal text-black/75">
+						{lowerTitle}
+					</span>
+				)}
+			</div>
+			{children}
+		</div>
+	)
+}
+
+export function TbCardContent({ className, children }: CardPT) {
+	return (
+		<div className={cn(className, "p-3 pt-0 text-left text-sm md:p-5 md:pt-0")}>
+			{children}
+		</div>
+	)
+}
+
+TbCardHeader.displayName = "TbCardHeader"
+TbCardContent.displayName = "TbCardContent"
+
+export type MiniCardPT = {
+	title: string
+	value?: string | number
+	unit?: string
+	amount?: number
+	isIncreased?: boolean
+	children?: React.ReactNode
+	className?: string
+}
+export function MiniCard({ title, children, className }: MiniCardPT) {
+	return (
+		<div
+			className={`bg-[#EFF6F1], w-full flex-col gap-2 rounded-xl p-6 shadow-md ${className}`}
+		>
+			<div className="text-xs font-semibold">{title}</div>
+			{children}
+		</div>
+	)
+}
+
+export function MiniCardContent({
+	value,
+	unit,
+	amount,
+	isIncreased,
+}: MiniCardPT) {
+	return (
+		<div className="flex items-center justify-between">
+			<div className="flex items-baseline gap-1">
+				<span className="scroll-m-20 text-2xl font-semibold tracking-tight">
+					{value}
+				</span>
+				<span className="scroll-m-20 text-lg font-semibold tracking-tight">
+					{unit}
+				</span>
+			</div>
+			{(isIncreased != null || "") &&
+				(isIncreased ? (
+					<div className="text-xs font-bold text-red-600">+{amount}</div>
+				) : (
+					<div className="text-xs font-bold text-blue-600">-{amount}</div>
+				))}
+		</div>
+	)
 }
