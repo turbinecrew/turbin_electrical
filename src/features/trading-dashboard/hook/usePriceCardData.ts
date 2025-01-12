35d65593a38dae843f578de78311a0b1c347190e
narim <0018823nmn@naver.com>
2025-01-13 04:20:28 +0900

    ⚡️: 카드 로딩, 에러 처리

--- src/features/trading-dashboard/hook/usePriceCardData.ts
+++ src/features/trading-dashboard/hook/usePriceCardData.ts
@@ -4,13 +4,14 @@ import { useTodaySMPData } from "@/features/trading-dashboard/hook/useTodaySMPDa
 export function usePriceCardData() {
 	const {
 		data: todaySMPData,
-		// isLoading: isSMPLoading,
-		// isError: isSMPError,
+		isLoading: isSMPLoading,
+		isError: isSMPError,
 	} = useTodaySMPData()
+
 	const {
 		data: todayRECData,
-		// isLoading: isRECLoading,
-		// isError: isRECError,
+		isLoading: isRECLoading,
+		isError: isRECError,
 	} = useTodayRECData()
 
 	// 가장 최근 데이터 추출
@@ -18,23 +19,20 @@ export function usePriceCardData() {
 		todaySMPData?.length > 0 ? todaySMPData[todaySMPData.length - 1] : null
 
 	const secondSMPData =
-		todaySMPData?.length > 0 ? todaySMPData[todaySMPData.length - 2] : null
+		todaySMPData?.length > 1 ? todaySMPData[todaySMPData.length - 2] : null
 
-	const SMPDataGap = () =>
+	const SMPDataGap =
 		recentSMPData?.Land && secondSMPData?.Land
 			? parseFloat((recentSMPData.Land - secondSMPData.Land).toFixed(2))
 			: null
 
-	// REC 데이터 추출
 	const recentRECData =
 		todayRECData?.length > 0 ? todayRECData[todayRECData.length - 1] : null
 
 	const secondRECData =
-		todayRECData?.length > 0 ? todayRECData[todayRECData.length - 2] : null
+		todayRECData?.length > 1 ? todayRECData[todayRECData.length - 2] : null
 
-	// 데이터 간의 차이 계산
-
-	const RECDataGap = () =>
+	const RECDataGap =
 		recentRECData?.closing_price && secondRECData?.closing_price
 			? parseFloat(
 					(recentRECData.closing_price - secondRECData.closing_price).toFixed(
@@ -44,33 +42,68 @@ export function usePriceCardData() {
 			: null
 
 	const mathSymbol = (number: number | null) => {
-		return number == null
-			? null
-			: number > 0
-				? true
-				: number == 0
-					? false
-					: null
+		if (number === null) return null
+		return number > 0 ? true : number === 0 ? false : null
 	}
 
-	const priceCardData = [
+	if (isSMPError || isRECError) {
+		return [
+			{
+				title: "현재 SMP 가격",
+				value: "error",
+				unit: "원/kWh",
+				isIncreased: null,
+				amount: null,
+				color: "bg-[#F6FCF3]",
+			},
+			{
+				title: "현재 REC 가격",
+				value: "error",
+				unit: "원/REC",
+				isIncreased: null,
+				amount: null,
+				color: "bg-[#EFF6F1]",
+			},
+		]
+	}
+
+	if (isSMPLoading || isRECLoading) {
+		return [
+			{
+				title: "현재 SMP 가격",
+				value: "-",
+				unit: "원/kWh",
+				isIncreased: null,
+				amount: null,
+				color: "bg-[#F6FCF3]",
+			},
+			{
+				title: "현재 REC 가격",
+				value: "-",
+				unit: "원/REC",
+				isIncreased: null,
+				amount: null,
+				color: "bg-[#EFF6F1]",
+			},
+		]
+	}
+
+	return [
 		{
 			title: "현재 SMP 가격",
 			value: recentSMPData ? recentSMPData.Land : "-",
 			unit: "원/kWh",
-			isIncreased: mathSymbol(SMPDataGap()),
-			amount: SMPDataGap(),
+			isIncreased: mathSymbol(SMPDataGap),
+			amount: SMPDataGap,
 			color: "bg-[#F6FCF3]",
 		},
 		{
 			title: "현재 REC 가격",
 			value: recentRECData ? recentRECData.closing_price : "-",
 			unit: "원/REC",
-			isIncreased: mathSymbol(RECDataGap()),
-			amount: RECDataGap(),
+			isIncreased: mathSymbol(RECDataGap),
+			amount: RECDataGap,
 			color: "bg-[#EFF6F1]",
 		},
 	]
-
-	return priceCardData
 }
